2584b02401cd50d32245b7e0fa4263ad69253801
Chris Shorrock <cs@stripe.com>
2020-11-22 14:25:39 -0800

    based templating

--- boilerplate.rb
+++ boilerplate.rb
@@ -1,156 +1,163 @@
+# typed: true
 # frozen_string_literal: true
-# Advent Boilerplate Start
+require 'sorbet-runtime'
 
 class String
+  extend T::Sig
+
+  sig {params(color_code: Integer).returns(String)}
   def colorize(color_code); "\e[#{color_code}m#{self}\e[0m"; end
-  def black; colorize(30); end
-  def red; colorize(31); end
-  def green; colorize(32); end
-  def yellow; colorize(33); end
-  def blue; colorize(34); end
-  def pink; colorize(35); end
-  def teal; colorize(36); end
-  def white; colorize(37); end
-  def bold; colorize(1); end
-  def dark; colorize(2); end
-  def italic; colorize(3); end
-  def underline; colorize(4); end
-  def white_background; colorize(7); end
-  def black_background; colorize(40); end    
-  def red_background; colorize(41); end    
-  def green_background; colorize(42); end    
-  def yellow_background; colorize(43); end    
-  def blue_background; colorize(44); end    
-  def pink_background; colorize(45); end    
-  def teal_background; colorize(46); end    
-  def white_background; colorize(47); end    
+
+  sig {returns(String)}; def black; colorize(30); end
+  sig {returns(String)}; def red; colorize(31); end
+  sig {returns(String)}; def green; colorize(32); end
+  sig {returns(String)}; def yellow; colorize(33); end
+  sig {returns(String)}; def blue; colorize(34); end
+  sig {returns(String)}; def pink; colorize(35); end
+  sig {returns(String)}; def teal; colorize(36); end
+  sig {returns(String)}; def white; colorize(37); end
+  sig {returns(String)}; def bold; colorize(1); end
+  sig {returns(String)}; def dark; colorize(2); end
+  sig {returns(String)}; def italic; colorize(3); end
+  sig {returns(String)}; def underline; colorize(4); end
+  sig {returns(String)}; def white_background; colorize(7); end
+  sig {returns(String)}; def black_background; colorize(40); end
+  sig {returns(String)}; def red_background; colorize(41); end
+  sig {returns(String)}; def green_background; colorize(42); end
+  sig {returns(String)}; def yellow_background; colorize(43); end
+  sig {returns(String)}; def blue_background; colorize(44); end
+  sig {returns(String)}; def pink_background; colorize(45); end
+  sig {returns(String)}; def teal_background; colorize(46); end
 end
 
 module Symbols
-  Block = '█'
+  BLOCK = '█'
 end
 
-module Loggable
-  attr_accessor :logging, :log_depth
+module AoC
+  extend T::Sig
 
-  def log(s, *args);
-    @log_depth ||= "  "
-    out = nil
+  sig {params(num: Integer).void}
+  def self.part(num)
+    puts "Part #{num}:".green; yield; puts ''
+  end
 
-    puts "#{@log_depth}#{s} #{args.map(&:to_s).join(", ")}" unless !@logging;
+  module IO
+    def self.input_file(strip: true, strip_newline: true)
+      return @input.dup unless @input.nil? # ARGV can only be used once
 
-    if block_given?
-      original_depth = @log_depth
-      @log_depth = @log_depth + "  "
-      begin
-        out = yield
-      ensure
-        @log_depth = original_depth
+      if ARGV.empty?
+        raise 'no input provided, please specify file as command line arg'
       end
+
+      @input ||= $<.map(&:to_s)
+      @input = if strip
+        @input.map(&:strip)
+      elsif strip_newline
+        @input.map {|i| i.gsub("\n", '')}
+      else
+        @input
+      end
+      @input = @input[0] if @input.length == 1
+      @input.dup # prevents alterations to source
     end
 
-    return out
-  end
-end
-
-class Boilerstate
-  include Loggable
-  attr_accessor :input, :options
-  def initialize(input, **options)
-    @options = options || {}
-    @logging = @options[:logging] || false
-    parse(input)
-  end
-
-  def parse(input)
-    raise NotImplementedError
-  end
-end
-
-def value_string(val)
-  value_str = val
-  value_str = '"' + value_str + '"' if val.is_a?(String)
-  value_str = 'nil' if val.nil?
-  value_str
-end
-
-def assert_equal(expect, value, description)
-  puts "  #{'✔'.green} #{description} == #{value_string(value)}" if value == expect
-  puts "  #{'✖'.red} #{description}: expected #{value_string(expect)}, received #{value_string(value)}" if value != expect
-  value
-end
-
-def assert_call(expect, *args)
-  assert_call_on(self, expect, *args)
-end
-
-def log_call(method, *args)
-  log_call_on(self, method, *args)
-end
-
-def assert_call_on(target, expect, *args)
-  log_call_on(target, *args) {|m, r, desc| assert_equal(expect, r, "#{m}(#{desc})")}
-end
-
-def log_call_on(target, method, *args)
-  arg_description = args.to_s[1...-1]
-  arg_description = "..." if arg_description.length > 10
-  method, arg_description = method if method.is_a?(Array)
-
-  result = target.send(method, *args)
-  return yield method, result, arg_description if block_given?
-  puts "  #{'-'.yellow} #{method}(#{arg_description}) == #{value_string(result)}"
-end
-
-def input(strip: true, strip_newline: true)
-  # ARGV can only be used once
-  return @input.dup unless @input.nil?
-
-  if ARGV.length == 0
-    raise "no input provided, please specify file as command line arg"
-  end
-
-  @input ||= $<.map(&:to_s)
-  @input = if strip
-    @input.map(&:strip)
-  elsif strip_newline
-    @input.map {|i| i.gsub("\n", '')}
-  else
-    @input
-  end
-  @input = @input[0] if @input.length == 1
-  @input.dup # prevents alterations to source
-end
-
-def read_stdin_char
-  begin
-    # save previous state of stty
-    old_state = `stty -g`
-    # disable echoing and enable raw (not having to press enter)
-    system "stty raw -echo"
-    c = STDIN.getc.chr
-    # gather next two characters of special keys
-    if(c=="\e")
-      extra_thread = Thread.new{
-        c = c + STDIN.getc.chr
-        c = c + STDIN.getc.chr
-      }
-      # wait just long enough for special keys to get swallowed
-      extra_thread.join(0.00001)
-      # kill thread so not-so-long special keys don't wait on getc
-      extra_thread.kill
+    def self.read_stdin_char
+      begin
+        # save previous state of stty
+        old_state = `stty -g`
+        # disable echoing and enable raw (not having to press enter)
+        system 'stty raw -echo'
+        c = T.must(STDIN.getc).chr
+        # gather next two characters of special keys
+        if c == "\e"
+          extra_thread = Thread.new do
+            c += T.must(STDIN.getc).chr
+            c += T.must(STDIN.getc).chr
+          end
+          # wait just long enough for special keys to get swallowed
+          extra_thread.join(0.00001)
+          # kill thread so not-so-long special keys don't wait on getc
+          extra_thread.kill
+        end
+      rescue => e
+        puts "#{e.class}: #{e.message}"
+        puts e.backtrace
+      ensure
+        # restore previous state of stty
+        system "stty #{old_state}"
+      end
+      c
     end
-  rescue => ex
-    puts "#{ex.class}: #{ex.message}"
-    puts ex.backtrace
-  ensure
-    # restore previous state of stty
-    system "stty #{old_state}"
   end
-  return c
+
+  class Logger
+    extend T::Sig
+
+    attr_accessor :logging
+    attr_accessor :log_depth
+
+    def initialize(logging: true)
+      @logging = logging
+      @log_depth = '  '
+    end
+
+    def log(str, *args)
+      out = nil
+
+      Kernel.puts "#{@log_depth}#{str} #{args.map(&:to_s).join(', ')}" unless @logging == false
+
+      if Kernel.block_given?
+        original_depth = @log_depth
+        @log_depth += '  '
+        begin
+          out = yield
+        ensure
+          @log_depth = original_depth
+        end
+      end
+
+      out
+    end
+  end
 end
 
-def part(num, &block)
-  puts "Part #{num.to_s}:".green; yield; puts ""
+DefaultLogger = AoC::Logger.new
+
+def log(str, *args)
+  DefaultLogger.log(str, *args)
 end
-# Advent Boilerplate End
+
+module Assert
+  extend T::Sig
+  include Kernel
+
+  def self.to_s(val)
+    value_str = val
+    value_str = '"' + value_str + '"' if val.is_a?(String)
+    value_str = 'nil' if val.nil?
+    value_str
+  end
+
+  sig {params(expect: T.untyped, value: T.untyped, description: String).returns(T.untyped)}
+  def self.equal(expect, value, description="")
+    puts "  #{'✔'.green} #{description} == #{Assert.to_s(value)}" if value == expect
+    puts "  #{'✖'.red} #{description}: expected #{Assert.to_s(expect)}, received #{Assert.to_s(value)}" if value != expect
+    value
+  end
+
+  def self.call(target, expect, *args)
+    T.unsafe(self).log_call(target, *args) {|m, r, desc| Assert.equal(expect, r, "#{m}(#{desc})")}
+  end
+
+  def self.log_call(target, method, *args)
+    arg_description = T.must(args.to_s[1...-1])
+    arg_description = '...' if arg_description.length > 10
+    method, arg_description = method if method.is_a?(Array)
+
+    result = target.send(method, *args)
+    return yield method, result, arg_description if block_given?
+
+    puts "  #{'-'.yellow} #{method}(#{arg_description}) == #{Assert.to_s(result)}"
+  end
+end
---
+++ template.rb
@@ -0,0 +1,11 @@
+# typed: true
+# frozen_string_literal: true
+require './boilerplate.rb'
+
+AoC.part 1 do
+  Assert.equal 1, 1, "basic numbers"
+end
+
+AoC.part 2 do
+  Assert.equal "foo", "bar"
+end
